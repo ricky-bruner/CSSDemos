b929748f3b6460bbd55f30d1bb0e24838da9392b
ricky-bruner <alteredecho07@yahoo.com>
2020-03-14 21:55:20 -0500

    added scrolling text demo

--- scripts.js
+++ scripts.js
@@ -46,6 +46,32 @@ let buildWavyCirclesDemo = () => {
     loader.innerHTML = spans;
 }
 
+//Build Scrolling Text Demo
+let buildScrollingBannerDemo = () => {
+    demoContainer.innerHTML = `<div class="scrolling-text"><section></section></div>`;
+    let container = document.querySelector('.scrolling-text');
+    let divs = `<div class="section-div"></div>`;
+    let h2s = `<h2></h2>`;
+    let lorem = "Half-giant jinxes peg-leg gillywater broken glasses large black dog Great Hall. Nearly-Headless Nick now string them together, and answer me this, which creature would you be unwilling to kiss? Poltergeist sticking charm, troll umbrella stand flying cars golden locket Lily Potter. Pumpkin juice Trevor wave your wand out glass orbs, a Grim knitted hats. Stan Shunpike doe patronus, suck his soul Muggle-Born large order of drills the trace. Bred in captivity fell through the veil, quaffle blue flame ickle diddykins Aragog. Yer a wizard, Harry Doxycide the woes of Mrs. Weasley Goblet of Fire."
+
+    for(let i = 0; i < 1; i++){
+        divs += `<div class="section-div"></div>`;
+    }
+
+    section = document.querySelector("section");
+    section.innerHTML = divs;
+    divs = document.querySelectorAll(".section-div");
+    divs.forEach(d => {
+        d.innerHTML = h2s;
+    })
+    h2s = document.querySelectorAll("h2");
+    h2s.forEach(h => {
+        h.innerText = lorem;
+    })
+
+}
+
+
 //Event Listeners for Menu Buttons
 document.querySelector('#under').addEventListener("click", () => {
     buildHexagonDemo("");
